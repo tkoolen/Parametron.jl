6395f1b32201e2b3eb81e0a55e110367a9483c0b
Robin Deits <robin.deits@gmail.com>
2018-07-05 17:14:29 -0400

    inbounds

--- src/lazyexpression.jl
+++ src/lazyexpression.jl
@@ -192,7 +192,7 @@ function optimize(expr::LazyExpression{typeof(adjoint)}, ::Type{<:AbstractMatrix
     LazyExpression(similar(deepcopy(expr())), expr.args...) do dest, A
         @boundscheck indices(A, 1) == indices(dest, 2) || throw(DimensionMismatch())
         @boundscheck indices(A, 2) == indices(dest, 1) || throw(DimensionMismatch())
-        for j in indices(A, 2)
+        @inbounds for j in indices(A, 2)
             for i in indices(A, 1)
                 dest[j, i] = A[i, j]
             end

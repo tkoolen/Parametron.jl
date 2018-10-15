0094d51db0c1236d15b3a7fb71137a9832ecd5ee
Twan Koolen <koolen.twan@gmail.com>
2018-10-15 16:30:14 -0400

    Deprecate MockModel()

--- src/mockmodel.jl
+++ src/mockmodel.jl
@@ -1,11 +1,4 @@
-struct MockModel
-    params::Vector{Parametron.Parameter}
-    nextvarindex::Base.RefValue{Int}
-end
-MockModel() = MockModel(Parametron.Parameter[], Ref(1))
-Parametron.setdirty!(model::MockModel) = foreach(Parametron.setdirty!, model.params)
-Parametron.addparameter!(model::MockModel, param::Parametron.Parameter) = (push!(model.params, param); param)
-Variable(model::MockModel) = (ret = Variable(model.nextvarindex[]); model.nextvarindex[] += 1; ret);
+Base.@deprecate MockModel() mock_model()
 
 function mock_model()
     optimizer = MOIU.MockOptimizer(Parametron.ParametronMOIModel{Float64}())
